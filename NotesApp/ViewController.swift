8aa3b4c68ed1e72d94245474695b01253024db7e
Telman Yusifov <43857129+Telmxn@users.noreply.github.com>
2025-05-16 19:29:01 +0400

    Initial commit.

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Telman Yusifov on 16.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
