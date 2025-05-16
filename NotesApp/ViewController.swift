d105cb1572b58a8a10f61a1be6a6c441ec02fd6e
Telman Yusifov <43857129+Telmxn@users.noreply.github.com>
2025-05-16 19:36:43 +0400

    Home UI is ready.

--- NotesApp/ViewController.swift
+++ NotesApp/ViewController.swift
@@ -6,15 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 16
+        view.distribution = .fillEqually
+        return view
+    }()
+    
+    private var noteTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Please enter your note..."
+        textField.layer.cornerRadius = 12
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        return textField
+    }()
+    
+    private var addNoteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add Note", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        setupUI()
     }
 
+    private func setupUI() {
+        view.addSubview(stackView)
+        
+        [noteTextField, addNoteButton].forEach(stackView.addArrangedSubview)
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        addNoteButton.snp.makeConstraints { make in
+            make.height.equalTo(48)
+        }
+    }
 
 }
 
